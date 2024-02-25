c392241f42dc4b8a16ee2bc327ab99769251ef7b
sandip <sandipicb@gmail.com>
2024-02-25 16:54:15 +0600

    timestamp

--- store/views.py
+++ store/views.py
@@ -81,4 +81,28 @@ def updateItem(request):
 
 def processOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['from']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+        
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+               state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+
+    else:
+        print("User is logged in...")
     return JsonResponse('Payment Complete', safe=False)
